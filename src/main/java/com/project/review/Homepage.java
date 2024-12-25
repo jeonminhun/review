a31e6e0c8d3b7a8a0c6ef0ffb507053724051dd2
jeonminhun <a01066215336@gmail.com>
2024-12-25 21:04:24 +0900

    타임리프 변수 추가 확인 및 컨트롤러 클래스 중복 수정

--- src/main/java/com/project/review/Homepage.java
+++ src/main/java/com/project/review/Homepage.java
@@ -1,16 +1,15 @@
 package com.project.review;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class HomepageNavigationController {
-    @Controller
-    public class WebController {
-
+@Controller
+public class Homepage {
         @RequestMapping("/")
-        public String main(){
-
+        public String main(Model model){
+            model.addAttribute("data","hi my name is review!");
             return "index";
         }
-    }
+
 }
