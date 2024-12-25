768e4f448ad106bd393d494bd90fe2b69d7d073e
jeonminhun <a01066215336@gmail.com>
2024-12-25 21:05:09 +0900

    새 상품 페이지 추가 및 컨트롤러 생성

---
+++ src/main/java/com/project/review/shop_item.java
@@ -0,0 +1,15 @@
+package com.project.review;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class shop_item {
+        @RequestMapping("/shop")
+        public String main(Model model){
+            model.addAttribute("data","hi my name is review!");
+            return "shop_item";
+        }
+
+}
