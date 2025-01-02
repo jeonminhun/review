9dbd12a35ed29a32854b56dc41fe854e5d51df21
jeonminhun <a01066215336@gmail.com>
2025-01-03 04:17:57 +0900

    쿼리 문제 확인용 테스트 api

--- src/main/java/com/project/review/user/service/userservice.java
+++ src/main/java/com/project/review/user/service/userservice.java
@@ -7,6 +7,9 @@ import com.project.review.user.dto.userCreateDto;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface userService {
+
+    boolean test(MemberRequestDto memberRequestDto);
+
     boolean checkPassWord(userCreateDto userCreateDto);
 
     boolean createUser(userCreateDto userCreateDto);
