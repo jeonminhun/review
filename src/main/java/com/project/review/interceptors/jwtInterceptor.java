fefc0a90b60cabc6fc7b60e03b13fe132d3e0cec
jeonminhun <a01066215336@gmail.com>
2025-01-04 06:59:46 +0900

    오류 수정

--- src/main/java/com/project/review/interceptors/jwtInterceptor.java
+++ src/main/java/com/project/review/interceptors/jwtInterceptor.java
@@ -29,44 +29,39 @@ public class jwtInterceptor implements HandlerInterceptor {
 
         Cookie[] cookies = request.getCookies(); // 모든 쿠키 가져오기
 
-        if (cookies != null) {
 
-            String time =  cookies[2].getValue();
+            if (cookies.length == 3) {
+                String time =  cookies[2].getValue();
 
-            long timestampLong = Long.parseLong(time);
-            Date date1 = new Date();
-            Date date2 = new Date(timestampLong);
-            long diffInMillies = Math.abs(date2.getTime()- date1.getTime());
-            diffInMillies = diffInMillies / (60 * 1000);
-            log.info("accessToken 유효시간이 "+diffInMillies+"분 남았습니다.");
+                long timestampLong = Long.parseLong(time);
+                Date date1 = new Date();
+                Date date2 = new Date(timestampLong);
+                long diffInMillies = Math.abs(date2.getTime()- date1.getTime());
+                diffInMillies = diffInMillies / (60 * 1000);
+                log.info("accessToken 유효시간이 "+diffInMillies+"분 남았습니다.");
 
-            if (5>=diffInMillies){ // 토큰 유효시간이 5분미만이면 갱신시작
-                log.info("accessToken 갱신 :"+ cookies[0].getValue());
-                for (Cookie c : cookies) {
-                    String name = c.getName(); // 쿠키 이름 가져오기
+                if (5>=diffInMillies){ // 토큰 유효시간이 5분미만이면 갱신시작
+                    log.info("accessToken 갱신 :"+ cookies[0].getValue());
+                    for (Cookie c : cookies) {
+                        String name = c.getName(); // 쿠키 이름 가져오기
 
-                    String value = c.getValue(); // 쿠키 값 가져오기
-                    if (name.equals("Authorization")) {
-                        if (StringUtils.hasText(value) && value.startsWith(BEARER_PREFIX)) {
-                            tokenRequestDto.setAccessToken(value.substring(6));
+                        String value = c.getValue(); // 쿠키 값 가져오기
+                        if (name.equals("Authorization")) {
+                            if (StringUtils.hasText(value) && value.startsWith(BEARER_PREFIX)) {
+                                tokenRequestDto.setAccessToken(value.substring(6));
+                            }
+                        }
+                        else if (name.equals("RefreshToken")) {
+                            if (StringUtils.hasText(value) && value.startsWith(BEARER_PREFIX)) {
+                                tokenRequestDto.setRefreshToken(value.substring(6));
+                            }
                         }
                     }
-                    else if (name.equals("RefreshToken")) {
-                        if (StringUtils.hasText(value) && value.startsWith(BEARER_PREFIX)) {
-                            tokenRequestDto.setRefreshToken(value.substring(6));
-                        }
+                    if (tokenRequestDto.getAccessToken()!=null&&tokenRequestDto.getRefreshToken()!=null){
+                        userController.reissue(tokenRequestDto,request,response);
                     }
-                }
-                if (tokenRequestDto.getAccessToken()!=null&&tokenRequestDto.getRefreshToken()!=null){
-                    userController.reissue(tokenRequestDto,request,response);
                 }
             }
-        }
-
-
-
-
-
         return true;
     }
 }
