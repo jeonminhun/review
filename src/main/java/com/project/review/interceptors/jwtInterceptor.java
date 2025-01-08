a9713aafe0d8fbd330565fca1eae9fa777da36f1
jeonminhun <a01066215336@gmail.com>
2025-01-09 02:48:16 +0900

    인터셉터 구조 개선

--- src/main/java/com/project/review/interceptors/jwtInterceptor.java
+++ src/main/java/com/project/review/interceptors/jwtInterceptor.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.util.WebUtils;
+
 import java.util.Date;
 import static com.project.review.user.jwt.JwtFilter.BEARER_PREFIX;
 
@@ -25,35 +27,33 @@ public class jwtInterceptor implements HandlerInterceptor {
         Cookie[] cookies = request.getCookies(); // 모든 쿠키 가져오기
 
         if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().contains("time")) {
-                    String time = cookie.getValue();
-                    long timestampLong = Long.parseLong(time);
-                    Date date1 = new Date();
-                    Date date2 = new Date(timestampLong);
-                    long diffInMillis = Math.abs(date2.getTime() - date1.getTime());
-                    diffInMillis = diffInMillis / (60 * 1000);
-                    log.info("accessToken 유효시간이 {}분 남았습니다.", diffInMillis);
-                    Cookie authorizationCookie = null;
-                    Cookie refreshTokenCookie = null;
-                    if (5 >= diffInMillis) { // 토큰 유효시간이 5분미만이면 갱신시작
-                        log.info("accessToken 갱신 :{}", cookies[0].getValue());
-                        for (Cookie c : cookies) {
-                            if (c.getName().equals("Authorization")) {
-                                authorizationCookie = c;
-                            } else if (c.getName().equals("RefreshToken")) {
-                                refreshTokenCookie = c;
-                            }
-                        }
-                        if (tokenRequestDto.getAccessToken() != null && tokenRequestDto.getRefreshToken() != null) {
-                            tokenRequestDto.setAccessToken(authorizationCookie.getValue().substring(6));
-                            tokenRequestDto.setRefreshToken(refreshTokenCookie.getValue().substring(6));
-                            userController.reissue(tokenRequestDto, request, response);
-                        }
-                    }
+            Cookie authCookieTime = WebUtils.getCookie(request, "time");
+            if (authCookieTime != null) {
+
+                String time = authCookieTime.getValue();
+                long timestampLong = Long.parseLong(time);
+
+                Date date1 = new Date();
+                Date date2 = new Date(timestampLong);
+
+                long diffInMillis = Math.abs(date2.getTime() - date1.getTime());
+                diffInMillis = diffInMillis / (60 * 1000);
+
+                log.info("accessToken 유효시간이 {}분 남았습니다.", diffInMillis);
+
+                if (5 >= diffInMillis) { // 토큰 유효시간이 5분미만이면 갱신시작
+                    log.info("accessToken 갱신 :{}", cookies[0].getValue());
+
+                    Cookie authCookie = WebUtils.getCookie(request, "Authorization");
+                    Cookie RefCookie = WebUtils.getCookie(request, "RefreshToken");
+                    tokenRequestDto.setAccessToken(authCookie.getValue().substring(6));
+                    tokenRequestDto.setRefreshToken(RefCookie.getValue().substring(6));
+                    userController.reissue(tokenRequestDto, request, response);
+
                 }
             }
         }
+
         long endTime = System.currentTimeMillis(); // 실행 종료 시간 측정
         log.info("jwtInterceptor 실행 시간: {} ms", (endTime - startTime));
         return true;
