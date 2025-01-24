9cd9931a7b526bcc7c2345772242bdde3e279994
jeonminhun <a01066215336@gmail.com>
2025-01-25 02:45:09 +0900

    추가 마이페이지 컨트롤러 작성

--- src/main/java/com/project/review/mypage/controller/myPageController.java
+++ src/main/java/com/project/review/mypage/controller/myPageController.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.WebUtils;
 
 @Controller
@@ -15,12 +17,32 @@ import org.springframework.web.util.WebUtils;
 public class myPageController {
 
     @GetMapping("/myPage")
+    public String myReview(Model model, HttpServletRequest request) {
+        Cookie authCookie = WebUtils.getCookie(request, "Authorization");
+        if (authCookie != null) {
+            return "after/myReview";
+        }
+        return "default/index";
+
+
+    }
+    @GetMapping("/myData")
     public String myPage(Model model, HttpServletRequest request) {
         Cookie authCookie = WebUtils.getCookie(request, "Authorization");
         if (authCookie != null) {
             return "after/myPage";
         }
-        return "after/myPage"; // 임시
+        return "default/index";
+
+
+    }
+    @GetMapping("/myWish")
+    public String myWish(Model model, HttpServletRequest request) {
+        Cookie authCookie = WebUtils.getCookie(request, "Authorization");
+        if (authCookie != null) {
+            return "after/myWishProduct";
+        }
+        return "default/index";
 
 
     }
