0d70ff8352908a8b3d4842b53d51426027442675
jeonminhun <a01066215336@gmail.com>
2025-01-29 01:18:54 +0900

    컨트롤러 생성

---
+++ src/main/java/com/project/review/admin/controller/adminController.java
@@ -0,0 +1,13 @@
+package com.project.review.admin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class adminController {
+    public String productInsert() {
+
+        return "redirect:/";
+    }
+}
