fe3fb9ee1944f05946b77581671873c5c17bc618
jeonminhun <a01066215336@gmail.com>
2025-01-30 01:15:45 +0900

    어드민 컨트롤러 수정

--- src/main/java/com/project/review/admin/controller/adminController.java
+++ src/main/java/com/project/review/admin/controller/adminController.java
@@ -1,12 +1,15 @@
 package com.project.review.admin.controller;
 
+import com.project.review.admin.dto.productCreateDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @Slf4j
 @Controller
-public class adminController {
-    public String productInsert() {
+public class adminController { // 사진 받는것도 추가 해야함 + userId 어떻게 할지(받을지) 생각하기
+    public String productInsert(@RequestBody productCreateDto productCreateDto) {
 
         return "redirect:/";
     }
