30003d1d447f56fb81fec9db561fb23fe3eab1c3
jeonminhun <a01066215336@gmail.com>
2025-01-31 03:09:02 +0900

    변수명 변경

--- src/main/java/com/project/review/product/entity/ProductImg.java
+++ src/main/java/com/project/review/product/entity/ProductImg.java
@@ -17,7 +17,7 @@ public class ProductImg {
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
-    private com.project.review.product.entity.product product;
+    private product product;
 
     @Column(nullable = false)
     private String product_img_name;
