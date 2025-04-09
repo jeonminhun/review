02f7e5f309ad8fa0ca62534a1253a1a056a4d7a4
jeonminhun <a01066215336@gmail.com>
2025-04-10 06:00:36 +0900

    깃 설정

--- src/main/java/com/project/review/ReviewApplication.java
+++ src/main/java/com/project/review/ReviewApplication.java
@@ -6,10 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.project.review.**.repository")
-@EnableRedisRepositories(basePackages = "com.project.review.user.redisRepository")
 public class ReviewApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewApplication.class, args);
 	}
