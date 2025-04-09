56473b0793fcd77e64b3b380fa34b7e1be179d65
jeonminhun <a01066215336@gmail.com>
2025-04-10 03:20:13 +0900

    깃 설정

--- src/main/java/com/project/review/user/repository/RefreshTokenRepository.java
+++ src/main/java/com/project/review/user/repository/RefreshTokenRepository.java
@@ -2,12 +2,13 @@ package com.project.review.user.repository;
 
 
 import com.project.review.user.entity.RefreshToken;
+import org.springframework.data.keyvalue.repository.KeyValueRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends KeyValueRepository<RefreshToken, String> {
     Optional<RefreshToken> findByKey(String key);
 }
