43597ff5a18736ca92f74ccf5e28d7c954c76f6f
jeonminhun <a01066215336@gmail.com>
2026-02-24 17:16:11 +0900

    제품 찜하기 버튼 피드백 개선

--- src/main/resources/static/assets/js/productSave.js
+++ src/main/resources/static/assets/js/productSave.js
@@ -2,6 +2,7 @@
         const productId = button.getAttribute("data-product-id");
         const userId = button.getAttribute("data-user-id");
         const isSaved = button.getAttribute("data-isSaved");
+        const isCurrentlySaved = button.classList.contains("saved");
 
         fetch(`/productSave`, {
             method: "POST",
@@ -15,13 +16,15 @@
             })
         }).then(data => {
               if (data != null) {
-                  alert("찜 성공!");
-
                    // 좋아요가 눌렸으면 버튼 상태 변경
-                  if (isSaved) {
+                  if (!isCurrentlySaved) {
+                    alert("찜 성공!");
                     button.classList.add("saved");
+                    button.innerHTML = '<i class="lni lni-heart-filled"></i> 찜하기';
                    } else {
+                    alert("찜 해제!");
                     button.classList.remove("saved");
+                    button.innerHTML = '<i class="lni lni-heart"></i> 찜하기';
                   }
 
                   // ✅ 강제로 DOM 다시 그리기 (리렌더링)
